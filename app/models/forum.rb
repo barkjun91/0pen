8cd63ad70af90cd3ed5e867f8f860a06730fb829
Goyange <goyange@bagyeongjunyi-macbook.local>
2009-07-18 17:20:01 +0900

    validation 추가

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -3,7 +3,9 @@ class Forum < ActiveRecord::Base
 	validates_uniqueness_of :name
   validates_presence_of :name
   validates_length_of :name, :in => 3..50
-  validates_format_of :name, :with => /^[-\w\d_.:]{3,50}$/
+  validates_format_of :name, :with => /^[-a-z0-9_.:]{3,50}$/i,
+                             :message => 'must be alphabets, digits, hyphens,' \
+                                         ' periods, underlines and colons.'
 	validates_presence_of :title
   validates_length_of :title, :maximum => 100
 end
