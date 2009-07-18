758ef6311d246ffdd5d62d514de9514c06d21fb0
Goyange <goyange@bagyeongjunyi-macbook.local>
2009-07-18 17:13:34 +0900

    validation 수정

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,6 +2,8 @@ class Forum < ActiveRecord::Base
   has_many :threads, :class_name => 'PostThread'
 	validates_uniqueness_of :name
   validates_presence_of :name
+  validates_length_of :name, :in => 3..50
+  validates_format_of :name, /^[-\w\d_.:]{3,50}$/
 	validates_presence_of :title
-  validates_presence_of :description
+  validates_length_of :title, :maximum => 100
 end
--- app/models/post_thread.rb
+++ app/models/post_thread.rb
@@ -2,4 +2,5 @@ class PostThread < ActiveRecord::Base
   belongs_to :forum
   has_many :posts, :foreign_key => :thread_id
   validates_presence_of :subject
+  validates_length_of :subject, :maximum => 150
 end
