bb4383a7b9555c5f9951bec365b1dc367f8d9e46
Goyange <goyange@bagyeongjunyi-macbook.local>
2009-07-18 17:14:52 +0900

    validation 수정

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -3,7 +3,7 @@ class Forum < ActiveRecord::Base
 	validates_uniqueness_of :name
   validates_presence_of :name
   validates_length_of :name, :in => 3..50
-  validates_format_of :name, /^[-\w\d_.:]{3,50}$/
+  validates_format_of :name, :with => /^[-\w\d_.:]{3,50}$/
 	validates_presence_of :title
   validates_length_of :title, :maximum => 100
 end
