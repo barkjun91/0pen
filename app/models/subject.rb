4ea372ba11033df92d5f5c7a80ff8a3399d4d2f2
Hong, MinHee <minhee@dahlia.kr>
2009-08-11 22:18:37 +0900

    디자인 조금.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -5,6 +5,13 @@ class Subject < ActiveRecord::Base
   validates_presence_of :title
   validates_length_of :title, :maximum => 150
 
+  def updated_at
+    last_revision = Revision.first(:joins => :post,
+                                   :conditions => ['posts.subject_id = ?', id],
+                                   :order => :created_at)
+    last_revision && last_revision.created_at
+  end
+
   def to_s
     title
   end
