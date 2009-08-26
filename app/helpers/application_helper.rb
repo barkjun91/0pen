8947c5766d9ebcc20cf72cfe7b93537e90ec5c9a
admire <khj983@lycos.co.kr>
2009-08-26 17:57:45 +0900

    Maruku추가

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
+require 'maruku' # $gem install maruku
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def markdown(text)
+    text.blank? ? "" : Maruku.new(text).to_html
+  end
 end
