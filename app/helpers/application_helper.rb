56075ede33468b2192a721edcd5bd8512acb3040
Hong, MinHee <minhee@dahlia.kr>
2009-10-12 13:24:43 +0900

    Markdown 처리기를 Maruku에서 Pandoku로 변경. http://github.com/dahlia/pandoku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ require 'openssl'
 require 'base64'
 
 module ApplicationHelper
+  MARKDOWN_FORMAT = ::Pandoku::Formats::Markdown.new(:smart => true)
+  HTML_FORMAT = ::Pandoku::Formats::HTML.new(:parse_raw => true,
+                                             :email_obfuscation => :javascript)
+
   def markdown(text)
-    text.blank? ? "" : Maruku.new(text).to_html
+    text.blank? ? '' : MARKDOWN_FORMAT.parse(text).compile(HTML_FORMAT)
   end
 
   def url_to_email(email)
